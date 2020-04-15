44359a5e4fa82ab12cfe611a13243227ba091491
Wagner Moura <wagner.moura@zoop.com.br>
2020-04-15 18:38:12 +0000

    motor de regras

---
+++ results/motorregrasstresssimulation-20200415183538667/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1586975741000,1],[1586975742000,1],[1586975743000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
